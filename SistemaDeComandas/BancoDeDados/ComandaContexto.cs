71654a015dfbf375f5d5e97092cde03df18ec747
KAUÃDEOLIVEIRALANGE <kauaoliveiralange9580@gmail.com>
2024-06-18 19:38:19 -0300

    ggg

---
+++ SistemaDeComandas/BancoDeDados/ComandaContexto.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using SistemaDeComandas.Modelos;
+
+namespace SistemaDeComandas.BancoDeDados
+{
+    public class ComandaContexto : DbContext
+    {
+        // criar as variaveis que representam tables
+        public DbSet<Mesa> Mesas { get; set; }
+        public DbSet<CardapioItem> CardapioItems { get; set; }
+        public DbSet<Comanda> Comandas { get; set; }
+        public DbSet<ComandaItem> ComandaItems { get; set; }
+        public DbSet<PedidoCozinha> PedidoCozinhas { get; set; }
+        public DbSet<PedidoCozinhaItem> PedidoCozinhaItems { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("DataSource=comandas.db");
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+
+        {
+            //acesso a entidade CardapioItem para determinar a nomenclatura
+            modelBuilder.Entity<CardapioItem>()
+                .HasComment("Cadastro de items do cardapio");
+
+            //Uma comanda possui muitos ComandasItems
+            //E suas chave estraingeira é ComandaId
+            modelBuilder.Entity<Comanda>()
+                .HasMany<ComandaItem>()
+                .WithOne(ci => ci.Comanda)
+                .HasForeignKey(f => f.ComandaId);
+            // o item de comanda possui um item de cardapio
+            // e sua chave estrangeira é CardapioItemId
+            modelBuilder.Entity<ComandaItem>()
+                .HasOne(ci => ci.CardapioItem)
+                .WithMany()
+                .HasForeignKey(ci => ci.CardapioItemId);
+
+            // Pedido cozinha com pedido cozinha item
+            modelBuilder.Entity<PedidoCozinha>()
+                .HasMany<PedidoCozinhaItem>()
+                .WithOne(pci => pci.PedidoCozinha)
+                .HasForeignKey(pci => pci.PedidoCozinhaId);
+
+            modelBuilder.Entity<PedidoCozinhaItem>()
+                .HasOne(pci => pci.ComandaItem)
+                .WithMany()
+                .HasForeignKey(pci => pci.ComandaItemId);
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+
+}
